f352adc8cbf908da84a0c12616eac6dad5b41417
danie <danie@DESKTOP-M2H76EP>
2019-09-23 15:34:57 +0200

    displaying data 1 week bef + 1 week after now

--- src/components/apicalls/actions/fetchDonePoints.js
+++ src/components/apicalls/actions/fetchDonePoints.js
@@ -1,15 +1,12 @@
+var today = new Date()
+var plusSeven = new Date(today.setDate(today.getDate() + 7));
+var minusSeven = new Date(today.setDate(today.getDate() - 31));
 
+var formatLastWeek = minusSeven.toISOString().split('T', 1)[0]
+var formatNextWeek = plusSeven.toISOString().split('T', 1)[0]
 
-var today = new Date(Date.now())
-var twoWeeks = new Date(Date.now() + 12096e5)
-console.log(today)
-console.log(twoWeeks)
-
-var formatToday = today.toISOString().split('T', 1)[0]
-var formatTwoWeeks = twoWeeks.toISOString().split('T', 1)[0]
-
-console.log(formatToday)
-console.log( formatTwoWeeks)
+// console.log(formatToday)
+// console.log( formatTwoWeeks)
 // function GetISODateFromEpochTimestamp(date) {
 //   var year = date.getFullYear();
 //   var month = date.getMonth() + 1;
@@ -33,10 +30,11 @@ console.log( formatTwoWeeks)
 
 export const fetchDonePoints = {
   method: 'GET',
-  endpoint: 'board/qDAFPals/burndownchartinfo/?startDate='+formatToday+'&endDate='+formatTwoWeeks,
+  endpoint: 'board/qDAFPals/burndownchartinfo/?startDate='+formatLastWeek+'&endDate='+formatNextWeek,
   headers: 
   {"Authorization": "Basic " + btoa("test:test")},
 
 }
+
 
 export default fetchDonePoints
