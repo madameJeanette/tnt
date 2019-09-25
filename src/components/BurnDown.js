388fda2dade70a98d80a761a329d52f5914f0889
danie <danie@DESKTOP-M2H76EP>
2019-09-25 14:31:19 +0200

    added TotalCards2Burndown

--- src/components/BurnDown.js
+++ src/components/BurnDown.js
@@ -7,6 +7,7 @@ import {
 const BurnDown = ({ loading, error, items, onReload }) => {
 
   items.items.forEach(item => {
+    item.TotalCards = item.totalItems
     item.ScrumPoints = item.totalDonePoint;
     item.BurnDownPoints = item.totalPoint - item.totalDonePoint;
     item.readableDate = new Date(item.date).toUTCString().split('2019', 1)[0];
@@ -22,8 +23,8 @@ const BurnDown = ({ loading, error, items, onReload }) => {
        <h3 >Tasks</h3>
      
      <LineChart
-      width={700}
-      height={400}
+      width={1000}
+      height={600}
       data= {items.items} //pass in data items from burndowncontainer
       margin={{
         top: 20, right: 50, left: 20, bottom: 20,
@@ -34,8 +35,9 @@ const BurnDown = ({ loading, error, items, onReload }) => {
       <YAxis />
       <Tooltip />
       <Legend />
-      <Line type="monotone" dataKey="BurnDownPoints" stroke="#1289A7" activeDot={{ r: 3 }} />
-      <Line type="monotone" dataKey="ScrumPoints" stroke="#ED4C67" activeDot={{ r: 3 }} /> 
+      <Line type="monotone" dataKey="BurnDownPoints" stroke="#ff9ff3" activeDot={{ r: 3 }} />
+      <Line type="monotone" dataKey="ScrumPoints" stroke="#12CBC4" activeDot={{ r: 3 }} /> 
+      <Line type="monotone" dataKey="TotalCards" stroke="#5758BB" activeDot={{ r: 3 }} /> 
       {/* Use data key from items */}    
     </LineChart>
     </div>
