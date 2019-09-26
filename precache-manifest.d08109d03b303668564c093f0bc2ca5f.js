2dc496e7e8f0b353e3cbbf0e3401544d82296ebb
danie <danie@DESKTOP-M2H76EP>
2019-09-26 11:43:26 +0200

    Updates

---
+++ precache-manifest.d08109d03b303668564c093f0bc2ca5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1924b2ec89665654803af7fe3e6bfbce",
+    "url": "/tnt/index.html"
+  },
+  {
+    "revision": "f81ec1f54f917330d15d",
+    "url": "/tnt/static/css/2.ff9ea1f9.chunk.css"
+  },
+  {
+    "revision": "8632f24e64e3017839ff",
+    "url": "/tnt/static/css/main.a65aef4d.chunk.css"
+  },
+  {
+    "revision": "f81ec1f54f917330d15d",
+    "url": "/tnt/static/js/2.2aa51dd4.chunk.js"
+  },
+  {
+    "revision": "8632f24e64e3017839ff",
+    "url": "/tnt/static/js/main.e58c63f2.chunk.js"
+  },
+  {
+    "revision": "661b4ce6ca15cf54591a",
+    "url": "/tnt/static/js/runtime~main.494e0876.js"
+  }
+]);
