9decd0b4176e5f3f1b308591b9f176dfef4d242d
danie <danie@DESKTOP-M2H76EP>
2019-09-26 14:17:01 +0200

    code clean up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React, { Suspense } from 'react';
 import Navbar from './components/layout/Navbar'
 import './App.css';
-// import {Route} from "react-router";
-// import { HashRouter, Route, Link } from "react-router-dom";
-
-// import Home from "./pages/Personal";
-// import Leaderboard from "./pages/Leaderboard";
-// import Team from "./pages/Team";
-// import DailyStandUp from './pages/DailyStandUp'
-
-// import {BrowserRouter as Router} from "react-router-dom";
-// import Settings from "./pages/Settings";
 import { ClientContextProvider } from 'react-fetching-library';
 import { Client } from './components/apicalls/Client';
 
@@ -24,22 +14,13 @@ function App() {
     <ClientContextProvider client={Client}> 
     <Suspense fallback={<span>Loading</span>}>
         <div className="App">
-            {/* <Router>
-               
-                <Route exact path="/" component={Home}/>
-                <Route path="/Leaderboard" component={Leaderboard}/>
-                <Route path="/Team" component={Team}/>
-                <Route path="/DailyStandUp" component={DailyStandUp}/>
-                <Route exact path="/Settings" component={Settings}/>  
-            </Router> */}
              <Navbar/>
         </div>
         </Suspense> 
         </ClientContextProvider>
     
-        // Routing for nav
         //suspense lazy loading added 
-        // Routing for github pages
+        // Routing for github pages moved to navbar
     );
     
 }
