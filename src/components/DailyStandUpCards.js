7d60405946d11735c4055d60849c01be29c30446
jaarieboii <jari.gantvoort97@gmail.com>
2019-10-02 13:45:21 +0200

    Deleted un used code
    Deleted some of the unused codes

--- src/components/DailyStandUpCards.js
+++ src/components/DailyStandUpCards.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Card, Icon } from 'semantic-ui-react'
-import _ from 'lodash'
+
 
 
 const CardList = (props) => {
@@ -34,30 +34,10 @@ const CardList = (props) => {
 
 const DailyStandUpCards = ({ loading, error, items, onReload }) => {
 
-
-    // items.forEach(item => {
-    //     console.log(item.name)
-    //     item.title = item.name
-    //     item.labels.forEach(label => {
-    //         console.log(label)
-        
-    //     })
-    //     item.members.forEach(member => {
-    //         console.log(member)
-    //     })
-
-    // })
     const Carditems = items.map((item) =>
         <CardList key={item.id} Header={item.name} members={item.members} labels={item.labels} cardURl={item.url}/>
     )
-    
-    
-    const x = items.map((item) =>
-        item.labels.filter((v, i) => item.labels.indexOf(v) === i))
-    
-    console.log(x)
-    
-    //console.log(items)
+
     return(
         <div>
            {Carditems}
